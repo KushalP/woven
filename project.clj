79b41ae17bfcd686ae885a247e9b1aa702762658
Kushal Pisavadia <kushi.p@gmail.com>
2012-01-02 23:07:55 +0000

    Uppercase 'T' for Textile

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject woven "0.0.1-SNAPSHOT"
-  :description "A simple textile parser written in Clojure"
+  :description "A simple Textile parser written in Clojure"
   :dependencies [[org.clojure/clojure "1.3.0"]])
