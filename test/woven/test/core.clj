ba2aecc1d48e3ba49112bcafa954af5019d2ac49
Kushal Pisavadia <kushi.p@gmail.com>
2012-01-03 21:02:39 +0000

    Move heading test into one block
    No need for separation at such a fine-grained level. Instead re-use this
    block and contain it as the results are VERY related.

--- test/woven/test/core.clj
+++ test/woven/test/core.clj
@@ -3,22 +3,12 @@
          [textile heading-block heading-parse split-and-trim-lines]])
   (:use [clojure.test]))
 
-(deftest heading-1
-  (is (= "<h1>Heading 1</h1>" (textile "h1. Heading 1"))))
-
-(deftest heading-2
-  (is (= "<h2>Heading 2</h2>" (textile "h2. Heading 2"))))
-
-(deftest heading-3
-  (is (= "<h3>Heading 3</h3>" (textile "h3. Heading 3"))))
-
-(deftest heading-4
-  (is (= "<h4>Heading 4</h4>" (textile "h4. Heading 4"))))
-
-(deftest heading-5
-  (is (= "<h5>Heading 5</h5>" (textile "h5. Heading 5"))))
-
-(deftest heading-6
+(deftest headings
+  (is (= "<h1>Heading 1</h1>" (textile "h1. Heading 1")))
+  (is (= "<h2>Heading 2</h2>" (textile "h2. Heading 2")))
+  (is (= "<h3>Heading 3</h3>" (textile "h3. Heading 3")))
+  (is (= "<h4>Heading 4</h4>" (textile "h4. Heading 4")))
+  (is (= "<h5>Heading 5</h5>" (textile "h5. Heading 5")))
   (is (= "<h6>Heading 6</h6>" (textile "h6. Heading 6"))))
 
 (deftest heading-multiline
